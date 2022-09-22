0c65e23bf94203593f478b785d373496454ebcb7
Elliot Plant <elliotaplant@gmail.com>
2022-09-21 17:34:43 -0700

    Add logs for move_dial

--- poster/actuate.py
+++ poster/actuate.py
@@ -7,9 +7,13 @@ def move_dial(value, dial_config, servo_config):
     print('moving pin', dial_config['pin'], 'to value', value)
     GPIO.setup(dial_config['pin'], GPIO.OUT)
     pin = GPIO.PWM(dial_config['pin'], servo_config['pwmFrequencyHz'])
-    pin.start(0) # Why 0?
+    pin.start(3) # Why 3?
     duty_cycle = convert_to_duty_cycle(value, dial_config['range'], servo_config['dutyCycleRange'])
+    print('duty cycle', duty_cycle, dial_config['pin'], ".".join(dial_config['path']))
     pin.ChangeDutyCycle(duty_cycle)
+    sleep(1)
+    self.surf_quality_pin.ChangeDutyCycle(0)
+    time.sleep(1)
     pin.stop()
 
 def convert_to_duty_cycle(value, metric_range, servo_range):
