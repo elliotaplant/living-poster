cf04e6339ac4268f326ee35ce16f9a0316e4cfea
Elliot Plant <elliotaplant@gmail.com>
2022-09-21 17:37:27 -0700

    Use right variables

--- poster/actuate.py
+++ poster/actuate.py
@@ -11,8 +11,8 @@ def move_dial(value, dial_config, servo_config):
     duty_cycle = convert_to_duty_cycle(value, dial_config['range'], servo_config['dutyCycleRange'])
     print('duty cycle', duty_cycle, dial_config['pin'], ".".join(dial_config['path']))
     pin.ChangeDutyCycle(duty_cycle)
-    sleep(1)
-    self.surf_quality_pin.ChangeDutyCycle(0)
+    time.sleep(1)
+    pin.ChangeDutyCycle(0)
     time.sleep(1)
     pin.stop()
 
