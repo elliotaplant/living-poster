73034aaf1b7661746b4c2cf3821bbbdb0c947df1
Elliot Plant <elliotaplant@gmail.com>
2023-02-12 20:25:19 -0800

    Add ability to get subconditions and time

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,31 @@ import { SURF_CONDITIONS_KV_KEY, updateConditions } from './updateConditions';
 
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
-    if (request.url.includes('favicon')) {
+    const url = new URL(request.url);
+
+    if (url.pathname.includes('favicon')) {
       return new Response('Why are you looking for a favicon?');
-    } else if (request.url.includes('update')) {
+    } else if (url.pathname.includes('update')) {
       await updateConditions(env.LIVING_POSTER);
       return new Response('Conditions updated');
     }
 
-    const surfConditions = await env.LIVING_POSTER.get(SURF_CONDITIONS_KV_KEY);
+    const conditionsJson = await env.LIVING_POSTER.get(SURF_CONDITIONS_KV_KEY);
+    const beach = url.searchParams.get('beach');
+    if (beach && conditionsJson) {
+      const conditions = JSON.parse(conditionsJson);
+      if (beach in conditions) {
+        const subConditions = {
+          time: Date.now(),
+          conditions: conditions[beach],
+        };
+        return new Response(JSON.stringify(subConditions), {
+          headers: { 'content-type': 'application/json' },
+        });
+      }
+    }
 
-    return new Response(surfConditions, {
+    return new Response(conditionsJson, {
       headers: { 'content-type': 'application/json' },
     });
   },
