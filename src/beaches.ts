b18507871cc29ec663b32c29154c9688421d0d70
Elliot Plant <elliotaplant@gmail.com>
2023-02-12 20:19:26 -0800

    Fix stinson noaa id

--- src/beaches.ts
+++ src/beaches.ts
@@ -9,7 +9,7 @@ export const beaches = {
   },
   stinson: {
     surflineId: '5842041f4e65fad6a77089c1',
-    noaaId: '9414290',
+    noaaId: '9415020', // Actually Point Reyes
   },
   sunsetCliffs: {
     surflineId: '5842041f4e65fad6a7708840',
