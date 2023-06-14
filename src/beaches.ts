52d8d30adce7ea566a30456a3496c3d6c1f19fd9
Elliot Plant <elliotaplant@gmail.com>
2023-06-14 12:46:28 -0700

    Add tide and chesapeake

--- src/beaches.ts
+++ src/beaches.ts
@@ -13,25 +13,44 @@
  * Search for a buoy near the beach
  * Find the ID in the URL
  */
-export const beaches = {
+
+type Condition = 'wave' | 'wind' | 'water-temp' | 'tide';
+
+export const beaches: {
+  [key: string]: {
+    surflineId: string;
+    noaaId: string;
+    conditions: Condition[];
+  };
+} = {
   tourmaline: {
     surflineId: '5842041f4e65fad6a77088c4',
     noaaId: '9410170',
+    conditions: ['wave', 'wind', 'water-temp'],
   },
   blacks: {
     surflineId: '5842041f4e65fad6a770883b',
     noaaId: '9410170',
+    conditions: ['wave', 'wind', 'water-temp'],
   },
   stinson: {
     surflineId: '5842041f4e65fad6a77089c1',
     noaaId: '9414290', // Actually San Francisco
+    conditions: ['wave', 'wind', 'water-temp'],
   },
   sunsetCliffs: {
     surflineId: '5842041f4e65fad6a7708840',
     noaaId: '9410170',
+    conditions: ['wave', 'wind', 'water-temp'],
   },
   montara: {
     surflineId: '5842041f4e65fad6a77089c1',
     noaaId: '9413450', // Actually Monterey
+    conditions: ['wave', 'wind', 'water-temp'],
+  },
+  chesapeake: {
+    surflineId: '5842041f4e65fad6a7708a28', // Fisherman's Island
+    noaaId: '8577330', // Solomon's Island
+    conditions: ['tide', 'wind', 'water-temp'],
   },
 };
--- src/updateConditions.ts
+++ src/updateConditions.ts
@@ -1,15 +1,20 @@
 import { beaches } from './beaches';
 
 const SURFLINE_BASE_URL = 'https://services.surfline.com/kbyg/spots/forecasts';
-const WATER_TMP =
+const WATER_TMP_URL =
   'https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?date=latest&product=water_temperature&' +
   'datum=STND&time_zone=lst&units=english&application=surf_poster&format=json&station=';
+const TIDE_API_URL =
+  'https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?date=latest&product=water_level&' +
+  'datum=MHW&time_zone=lst&units=english&application=surf_poster&format=json&station=';
+
 export const SURF_CONDITIONS_KV_KEY = 'SURF_CONDITIONS';
 
 export interface BeachConditions {
   surfHeight: number;
   windSpeed: number;
   waterTemp: number;
+  tide: number;
 }
 
 async function getSurflineCondition(
@@ -38,7 +43,7 @@ async function getSurflineCondition(
 }
 
 async function getNoaaWaterTemp(noaaId: string) {
-  const noaaResponse = await fetch(WATER_TMP + noaaId);
+  const noaaResponse = await fetch(WATER_TMP_URL + noaaId);
 
   // Change the response headers because NOAA's response is not application/json so the .json method doesn't like it
   const withUpdatedHeaders = new Response(noaaResponse.body, {
@@ -50,29 +55,60 @@ async function getNoaaWaterTemp(noaaId: string) {
   });
 
   const parsedNoaaResponse: any = await withUpdatedHeaders.json();
-  console.log(
-    'parsedNoaaResponse',
-    JSON.stringify(parsedNoaaResponse, null, 2)
-  );
+  return Number(parsedNoaaResponse.data[0].v);
+}
+
+async function getNoaaTide(noaaId: string) {
+  const noaaResponse = await fetch(TIDE_API_URL + noaaId);
+
+  // Change the response headers because NOAA's response is not application/json so the .json method doesn't like it
+  const withUpdatedHeaders = new Response(noaaResponse.body, {
+    ...noaaResponse,
+    headers: new Headers({
+      ...noaaResponse.headers,
+      'content-type': 'application/json',
+    }),
+  });
+
+  const parsedNoaaResponse: any = await withUpdatedHeaders.json();
   return Number(parsedNoaaResponse.data[0].v);
 }
 
 async function getSurfConditions() {
   const beachConditions: { [beachName: string]: BeachConditions } = {};
 
-  for (const [beachName, { surflineId, noaaId }] of Object.entries(beaches)) {
-    beachConditions[beachName] = { surfHeight: 0, windSpeed: 0, waterTemp: 0 };
+  for (const [beachName, { surflineId, noaaId, conditions }] of Object.entries(
+    beaches
+  )) {
+    beachConditions[beachName] = {
+      surfHeight: 0,
+      windSpeed: 0,
+      waterTemp: 0,
+      tide: 0,
+    };
 
-    // Get wave height and wind speed from surfline
-    console.log(beachName, surflineId);
-    const { surf } = await getSurflineCondition('wave', surflineId);
-    beachConditions[beachName].surfHeight = (surf.max + surf.min) / 2;
+    try {
+      if (conditions.includes('wave')) {
+        // Get wave height and wind speed from surfline
+        const { surf } = await getSurflineCondition('wave', surflineId);
+        beachConditions[beachName].surfHeight = (surf.max + surf.min) / 2;
+      }
 
-    const { speed, gust } = await getSurflineCondition('wind', surflineId);
-    beachConditions[beachName].windSpeed = (speed + gust) / 2;
+      if (conditions.includes('wind')) {
+        const { speed, gust } = await getSurflineCondition('wind', surflineId);
+        beachConditions[beachName].windSpeed = (speed + gust) / 2;
+      }
 
-    // Get water temp from noah
-    beachConditions[beachName].waterTemp = await getNoaaWaterTemp(noaaId);
+      if (conditions.includes('water-temp')) {
+        beachConditions[beachName].waterTemp = await getNoaaWaterTemp(noaaId);
+      }
+
+      if (conditions.includes('tide')) {
+        beachConditions[beachName].tide = await getNoaaTide(noaaId);
+      }
+    } catch (error) {
+      console.error('Failed to get conditions for beach', { beachName }, error);
+    }
   }
 
   return beachConditions;
